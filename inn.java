57bf89680cff54f51a4e75d4e9a87ce86dea8841
shauibi <352449622@qq.com>
2023-03-31 19:01:01 +0800

    增加了i 值计算

--- inn.java
+++ inn.java
@@ -1,6 +1,9 @@
+
 public class inn{
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("nishishabim");
+        int i = 100;
+        System.out.println(i*100);
     }
 }
